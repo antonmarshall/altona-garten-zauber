9726c986d5c1dc7a1be71790c079fa4ce59d3713
antonmarshall <anton.marshall03@gmail.com>
2025-05-14 04:30:51 +0200

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/altona-garten-zauber">
+      <BrowserRouter basename="/altona-garten-zauber/">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
